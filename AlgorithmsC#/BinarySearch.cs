af725f7993cb58d33e286a6c99f97ba162226f92
Marcello <marcellocavazzaoliveira.dev@gmail.com>
2024-01-03 08:16:44 -0300

    Making propmt

---
+++ AlgorithmsC#/BinarySearch.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsC_
+{
+    public class BinarySearch
+    {
+
+        public BinarySearch() {
+            int[] arr = new int[1000000];
+            int arraySize = arr.Length;
+            for (int i = 0; i < arraySize; i++)
+            {
+                arr[i] = i + 1;
+            }
+            var watch = Stopwatch.StartNew();
+            int target = 45670, targetIndex = -1, currentSize = arraySize;
+            int result = BinarySearchMethod(0, currentSize, target, arr);
+            Console.WriteLine(result);
+
+            watch.Stop();
+            Console.WriteLine(
+                $"The Execution time of the program is {watch.ElapsedMilliseconds}ms");
+        }
+
+        public int BinarySearchMethod(int minSize, int maxSize, int targetValue, int[] array)
+        {
+            decimal indexToCompare = -1;
+
+            decimal middleValue = (minSize + maxSize) / 2;
+
+            if (middleValue % 1 == 0)
+            {
+                if (array[(int)middleValue - 1] == targetValue)
+                {
+                    return (int)middleValue - 1;
+                }
+                else if (array[(int)middleValue - 1] > targetValue)
+                {
+                    return BinarySearchMethod(minSize, (int)middleValue, targetValue, array);
+                }
+                return BinarySearchMethod((int)middleValue, maxSize, targetValue, array);
+            }
+            decimal leftValue = middleValue - 0.5m;
+            decimal rightValue = middleValue + 0.5m;
+            if (array[(int)leftValue] == targetValue)
+            {
+                return (int)leftValue;
+            }
+            if (array[(int)rightValue] == targetValue)
+            {
+                return (int)rightValue;
+            }
+            else if (array[(int)leftValue - 1] > targetValue)
+            {
+                return BinarySearchMethod(minSize, (int)rightValue, targetValue, array);
+            }
+            return BinarySearchMethod((int)rightValue, maxSize, targetValue, array);
+        }
+
+    }
+
+}
--- AlgorithmsC#/Program.cs
+++ AlgorithmsC#/Program.cs
@@ -1,51 +1,33 @@
-﻿using System.Diagnostics;
+﻿using AlgorithmsC_;
+using System.Diagnostics;
 using System.Reflection.Metadata.Ecma335;
 
-int[] arr = new int[1000000];
-int arraySize = arr.Length;
-for (int i = 0; i < arraySize; i++)
+int value = 0;
+
+while (value == 0)
 {
-    arr[i] = i + 1;
+    Console.WriteLine("Select an option:\n" +
+    "1- Exit\n" +
+    "2- Binary Search\n");
+    string answer = Console.ReadLine();
+
+    Int32.TryParse(answer, out value);
+
+    switch (value)
+    {
+        case 1:
+            Console.Clear();
+            break;
+        case 2:
+            Console.Clear();
+            BinarySearch a = new BinarySearch();
+            value = 0;
+            Console.WriteLine("\nPress any key to continue");
+            Console.ReadKey();
+            Console.Clear();
+            break;
+        default:
+            Console.Clear();
+            break;
+    }
 }
-var watch = Stopwatch.StartNew();
-int target = 45670, targetIndex = -1, currentSize = arraySize;
-int result = BinarySearch(0, currentSize, target, arr);
-Console.WriteLine(result);
-static int BinarySearch(int minSize, int maxSize, int targetValue, int[] array)
-{
-    decimal indexToCompare = -1;
-
-    decimal middleValue = (minSize+maxSize)/2;
-
-    if (middleValue % 1 == 0)
-    {
-        if(array[(int)middleValue-1] == targetValue) 
-        {
-            return (int)middleValue - 1;
-        }
-        else if(array[(int)middleValue - 1] > targetValue)
-        {
-            return BinarySearch(minSize, (int)middleValue, targetValue, array);
-        }
-        return BinarySearch((int)middleValue, maxSize, targetValue, array);
-    }
-    decimal leftValue = middleValue - 0.5m;
-    decimal rightValue = middleValue + 0.5m;
-    if (array[(int)leftValue] == targetValue)
-    {
-        return (int)leftValue;
-    }
-    if (array[(int)rightValue] == targetValue)
-    {
-        return (int)rightValue;
-    }
-    else if (array[(int)leftValue - 1] > targetValue)
-    {
-        return BinarySearch(minSize, (int)rightValue, targetValue, array);
-    }
-    return BinarySearch((int)rightValue, maxSize, targetValue, array);
-}
-
-watch.Stop();
-Console.WriteLine(
-    $"The Execution time of the program is {watch.ElapsedMilliseconds}ms");
